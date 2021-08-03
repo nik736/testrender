1dc638ae94e2ecf22db59ed7188b410f69431983
Niklas Karoly <niklas@karoly.io>
2021-08-03 11:15:51 +0200

    test3

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      Test12345
+      Test123456
     </p>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
